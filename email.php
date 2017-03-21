cc29583bc25214ae9951ad5f57cce695ce39d439
JustinLomelino <justin.lomelino@gmail.com>
2017-03-21 11:41:32 -0500

    Updated email.php to fix some issues with display
    Removed the blank message details before a message is sent.
    Also pointed the form action to 'email.php' instead of 'action.php'

--- email.php
+++ email.php
@@ -13,7 +13,7 @@ UseSTARTTLS=YES
 -->
 
 <h2>A form to send email using sstmp</h2>
-<form action="action.php" method="post">
+<form action="email.php" method="post">
 	<p>First Name: <input type="text" name="firstname" /></p>
 	<p>Last Name: <input type="text" name="lastname" /></p>
 	<p>Recipient Email: <input type="text" name="rEmail" /></p>
@@ -22,24 +22,37 @@ UseSTARTTLS=YES
 	<p><input type="submit" name="submit" /><input type="reset"/></p>
 </form>
 
-<br><br>
-<h3>Message Details</h3>
-From: <?php echo htmlspecialchars($_POST['firstname']);?> <?php echo htmlspecialchars($_POST['lastname']); ?> <br>
-To: <?php echo htmlspecialchars($_POST['rEmail']);?> <br>
-Subject: <?php echo htmlspecialchars($_POST['subject']);?> <br>
-Message: <?php echo htmlspecialchars($_POST['message']);?> <br>
-
 <?php
-error_reporting(E_ALL);
+	if(count($_POST) > 0 && isset($_POST['submit'])){
+		echo "<br><br>";
+		echo "<h3>Message Details</h3>";
+		echo "From: ";
+		echo htmlspecialchars($_POST['firstname']);
+		echo " ";
+		echo htmlspecialchars($_POST['lastname']);
+		echo "<br>";
+		echo "To: ";
+		echo htmlspecialchars($_POST['rEmail']);
+		echo "<br>";
+		echo "Subject: ";
+		echo htmlspecialchars($_POST['subject']);
+		echo "<br>";
+		echo "Message: ";
+		echo htmlspecialchars($_POST['message']);
+		echo "<br>";
+	}
 
-function sendMail() {
 
-	echo '<br>email sent.<br>';
-	exec("echo -e 'From: ".$_POST['firstname']." ".$_POST['lastname']."\n".$_POST['message']."' | mail -s '".$_POST['subject']."' ".$_POST['rEmail']." ");
+	error_reporting(E_ALL);
 
-}
+	function sendMail() {
 
-if(count($_POST) > 0 && isset($_POST['submit'])){
-	sendMail();
-}
+		echo '<br>email sent.<br>';
+		exec("echo -e 'From: ".$_POST['firstname']." ".$_POST['lastname']."\n".$_POST['message']."' | mail -s '".$_POST['subject']."' ".$_POST['rEmail']." ");
+
+	}
+
+	if(count($_POST) > 0 && isset($_POST['submit'])){
+		sendMail();
+	}
 ?>
